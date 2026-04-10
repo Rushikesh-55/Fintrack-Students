c3158211dcc23afd1dad8867c1c1f3046d539e4a
Rushikesh-55 <vrushikeshnaidu@gmail.com>
2026-04-10 17:24:37 +0530

    Add local storage feature

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 let total = 0;
+
+function render() {
+  let list = document.getElementById("list");
+  list.innerHTML = "";
+  total = 0;
+
+  expenses.forEach(e => {
+    total += e.amount;
+
+    let li = document.createElement("li");
+    li.innerText = `${e.desc} - ₹${e.amount} (${e.category})`;
+
+    list.appendChild(li);
+  });
+
+  document.getElementById("total").innerText = total;
+}
 
 function addExpense() {
   let desc = document.getElementById("desc").value;
   let amount = Number(document.getElementById("amount").value);
+  let category = document.getElementById("category").value;
 
   if (!desc || !amount) return;
 
-  total += amount;
-  document.getElementById("total").innerText = total;
+  expenses.push({ desc, amount, category });
+  localStorage.setItem("expenses", JSON.stringify(expenses));
 
-  let li = document.createElement("li");
-  li.innerText = desc + " - ₹" + amount;
-
-  document.getElementById("list").appendChild(li);
+  render();
 }
+
+render();
